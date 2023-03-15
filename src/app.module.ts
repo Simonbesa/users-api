5a87f62b0843db832d1023145ec4409d6805af51
Simón Silva <ssilvaa@creditu.com>
2023-03-15 00:13:29 -0300

    configura docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HealthModule } from './health/health.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
     AuthModule,
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UsersModule,
